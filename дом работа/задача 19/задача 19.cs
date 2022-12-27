67a27ab90e951e0f1ce76e70fdabc3253ca587f6
marina140291 <marina140291@mail.ru>
2022-12-28 00:10:20 +0300

    Задача 19

---
+++ дом работа/задача 19/задача 19.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+while (x < 10000 || x > 99999)
+{
+ Console.WriteLine("вы ошиблись, введите число");
+ x = Convert.ToInt32(Console.ReadLine());
+ } 
+int a = x / 10000;
+int b = (x / 1000) % 10;
+int c = (x / 10) % 10;
+int d = x % 10;
+
+if (a == d && b == c)
+Console.WriteLine("да");
+else 
+Console.WriteLine("нет");
+
