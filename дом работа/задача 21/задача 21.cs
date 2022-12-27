c6ad1be99bd5e67e9a73238a95e5fd7809bb4b66
marina140291 <marina140291@mail.ru>
2022-12-28 00:25:54 +0300

    Задача 21

---
+++ дом работа/задача 21/задача 21.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите координату Х1: ");
+double x1 = Convert.ToDouble(Console.ReadLine()); 
+Console.WriteLine("Введите координату Y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine()); 
+Console.WriteLine("Введите координату Z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату Х2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату Y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату Z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());  
+double X = (x2-x1)*(x2-x1);
+double Y = (y2-y1)*(y2-y1);
+double Z = (z2-y1)*(z2-y1);
+double S = Math.Sqrt(X + Y + Z);
+Console.WriteLine(S);
+
